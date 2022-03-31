0eda3332629ba57e3b135659a4e81f65e2b28488
Vuyun01 <vu12401it@gmail.com>
2022-03-31 17:02:44 +0700

    new update

--- demo_git_2.py
+++ demo_git_2.py
@@ -1,3 +1,4 @@
 print('git branch <name branch> : create a new branch'),
 print('git checkout <name branch> : switch to the <name branch> branch'),
 print('git checkout -b <name branch> : create a new branch and set it to main branch'),
+print('git branch -d <name branch>: delete a branch')
