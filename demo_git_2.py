2769e2f691f5c00de556f9062c40044e3d6cc6b0
Vuyun01 <vu12401it@gmail.com>
2022-03-31 16:41:50 +0700

    How to add a newa branch

---
+++ demo_git_2.py
@@ -0,0 +1,3 @@
+print('git branch <name branch> : create a new branch'),
+print('git checkout <name branch> : switch to the <name branch> branch'),
+print('git checkout -b <name branch> : create a new branch and set it to main branch'),
